75c7972fba39f6804a53b192d4e0470bd169e2a9
symberprzemyslaw <symber.przemyslaw@gmail.com>
2023-09-16 10:59:33 +0200

    Display pomodoro counter update

--- app/src/Counter.jsx
+++ app/src/Counter.jsx
@@ -1,34 +1,26 @@
-import  { useState, useEffect } from 'react';
-  
-const Counter = ({play, time, setTime}) => {
-    let minutes = Math.floor(time / 60).toLocaleString('en-US', {
-        minimumIntegerDigits : 2,
-        useGrouping : false
-    }) 
-    let seconds = Math.floor(time % 60).toLocaleString('en-US', {
-        minimumIntegerDigits : 2,
-        useGrouping : false
-    }) 
-  
-    useEffect(() => { // przeniesienie tej funkcji do display? uzycie false/true do naliczania
-  
-        if(time !== 0){
-        //Implementing the setInterval method
-        const interval = setInterval(() => {
+import { useEffect } from 'react';
 
-                setTime(time - 1);
-                console.log('hi')
-            }, 1000);
-            return () => clearInterval(interval);
-        } else {
-            play()
-        }
-  
-        //Clearing the interval
-    }, [time]);
-    // must add some number formating
-    return <h1>{minutes} : {seconds}</h1>;
-}
+const Counter = ({ time, setTime, counting }) => {
+  let minutes = Math.floor(time / 60).toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  let seconds = Math.floor(time % 60).toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
 
+  useEffect(() => {
+    if (counting && time !== 0) {
+      //Implementing the setInterval method
+      const interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [time, counting]);
+
+  return <h1 className='timer'>{minutes} : {seconds}</h1>;
+};
 
 export default Counter;
--- app/src/Display.jsx
+++ app/src/Display.jsx
@@ -1,57 +1,63 @@
 import { useState, useEffect } from "react";
-import  Counter  from "./Counter"
-import ring from './assets/ring.mp3'
+import Counter from "./Counter";
+import ring from './assets/ring.mp3';
 
 const Display = () => {
-    const [time, setTime] = useState((2 * 60))
-    const [action, setAction] = useState('Start')
-    const [count, setCount] = useState('#0')
-    const container = document.querySelector(".container")
-    const play = () => new Audio(ring).play()
-    console.log(container)
+  const [time, setTime] = useState(1 * 60);
+  const [action, setAction] = useState('Start');
+  const [count, setCount] = useState(0);
+  const [counting, setCounting] = useState(false);
+  const [isPomodoroSet, setPomodoro] = useState(true);
 
+  const container = document.querySelector(".container");
+  const play = () => new Audio(ring).play();
+  const reset = () => {
+    setCounting(false);
+    setAction('Start');
+  };
 
-    const handleTime = () => {
-        // divide by 60 seconds + remaider
-        if( action === 'Start'){
-            setAction('Pause')
-        } else {
-            setAction('Start')
-        }
-
-        // if(time !== 0) {
-        //     useEffect( () => {
-
-        //         setInterval( () => {
-        //             setTime(time - 1)
-        //         },1000)
-                
-        //     },[time]
-        //     )
-        // }
+  const handleTime = () => {
+    if (action === 'Start') {
+      setAction('Pause');
+    } else {
+      setAction('Start');
     }
-    return ( 
-        <div className="display">
-            <button onClick={ () => {
-                container.style.backgroundColor = "rgb(186, 73, 73)"
-                setTime(25 * 60)
-            }}>Pomodoro</button>
-            <button onClick={ () => {
-                container.style.backgroundColor = "rgb(56, 133, 138)"
-                setTime(5 * 60)
-            }}>Short Break</button>
-            <button onClick={ () => {
-                container.style.backgroundColor = "rgb(57, 112, 151)"
-                setTime(15 * 60)
-            }}>Long Break</button>
-            <Counter time={time} setTime={setTime} play={play}/>
-            <div className="timer">{time}</div>
-            <button onClick={handleTime}>{action}</button>
-            <div className="count">
-                {count}
-            </div>
-        </div>
-     );
-}
- 
+
+    setCounting((prevCounting) => !prevCounting);
+  };
+
+  useEffect(() => {
+    if (time === 0 && isPomodoroSet) {
+      play();
+      setCount((prevCount) => prevCount + 1);
+      reset();
+    }
+  }, [time]);
+
+  return (
+    <div className="display">
+      <button onClick={() => {
+        container.style.backgroundColor = "rgb(186, 73, 73)";
+        setTime(25 * 60);
+        setPomodoro(true)
+      }}>Pomodoro</button>
+      <button onClick={() => {
+        container.style.backgroundColor = "rgb(56, 133, 138)";
+        setTime(5 * 60);
+        setPomodoro(false)
+      }}>Short Break</button>
+      <button onClick={() => {
+        container.style.backgroundColor = "rgb(57, 112, 151)";
+        setTime(15 * 60);
+        setPomodoro(false)
+      }}>Long Break</button>
+      <Counter play={play} time={time} setTime={setTime} counting={counting} />
+      <button onClick={handleTime}>{action}</button>
+      <div className="count">
+        #{count}
+      </div>
+    </div>
+  );
+};
+
 export default Display;
